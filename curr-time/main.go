f8da42bc22190b1d74e43c68999008c983bcce92
Mateusz <siiikor.sikorski@gmail.com>
2024-11-23 20:42:32 +0100

    ex 3 solution

--- curr-time/main.go
+++ curr-time/main.go
@@ -1,37 +1,72 @@
 package main
 
 import (
+	"encoding/json"
 	"log/slog"
 	"net/http"
 	"time"
 )
 
 func main() {
-    handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        currentTime := time.Now()
-        rfcTime := currentTime.Format(time.RFC3339)
-        w.Write([]byte(rfcTime))
-    })
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		currentTime := time.Now()
+		rfcTime := currentTime.Format(time.RFC3339)
 
-    loggingMiddleware := func(h http.Handler) http.Handler {
-        return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-            slog.Info("Received request", "from", r.RemoteAddr)
-            h.ServeHTTP(w, r) 
-        })
-    }
+		var output []byte
+		acceptHeader := r.Header.Get("Accept")
+		if acceptHeader == "application/json" {
+			w.Header().Set("Content-Type", "application/json")
 
-    mux := http.NewServeMux()
-    mux.Handle("GET /time", loggingMiddleware(handler))
+			res := struct {
+				DayOfWeek  string `json:"day_of_week"`
+				DayOfMonth int    `json:"day_of_month"`
+				Month      string `json:"month"`
+				Year       int    `json:"year"`
+				Hour       int    `json:"hour"`
+				Minute     int    `json:"minute"`
+				Second     int    `json:"second"`
+			}{
+				DayOfWeek:  currentTime.Weekday().String(),
+				DayOfMonth: currentTime.Day(),
+				Month:      currentTime.Month().String(),
+				Year:       currentTime.Year(),
+				Hour:       currentTime.Hour(),
+				Minute:     currentTime.Minute(),
+				Second:     currentTime.Second(),
+			}
 
-    s := http.Server{
-        Addr: ":8080",
-        Handler: mux,
-    }
+			out, err := json.Marshal(res)
+			if err != nil {
+				panic(err)
+			}
+			output = out
+		} else {
+			w.Header().Set("Content-Type", "text/plain")
+			output = []byte(rfcTime)
+		}
+        w.WriteHeader(http.StatusOK)
+		w.Write(output)
+	})
 
-    slog.Info("Starting to listen on", "port", s.Addr)
+	loggingMiddleware := func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			slog.Info("Received request", "from", r.RemoteAddr)
+			h.ServeHTTP(w, r)
+		})
+	}
 
-    err := s.ListenAndServe()
-    if err != nil {
-        panic(err)
-    }
+	mux := http.NewServeMux()
+	mux.Handle("GET /time", loggingMiddleware(handler))
+
+	s := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	slog.Info("Starting to listen on", "port", s.Addr)
+
+	err := s.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
 }
