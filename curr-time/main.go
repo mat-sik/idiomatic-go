d14961887a599490301b976d698b27e8e1b1b89b
Mateusz <siiikor.sikorski@gmail.com>
2024-11-23 12:34:58 +0100

    Add logging middleware

--- curr-time/main.go
+++ curr-time/main.go
@@ -1,24 +1,34 @@
 package main
 
 import (
+	"log/slog"
 	"net/http"
 	"time"
 )
 
 func main() {
-    h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
         currentTime := time.Now()
         rfcTime := currentTime.Format(time.RFC3339)
         w.Write([]byte(rfcTime))
     })
 
+    loggingMiddleware := func(h http.Handler) http.Handler {
+        return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+            slog.Info("Received request", "from", r.RemoteAddr)
+            h.ServeHTTP(w, r) 
+        })
+    }
+
     mux := http.NewServeMux()
-    mux.Handle("GET /time", h)
+    mux.Handle("GET /time", loggingMiddleware(handler))
 
     s := http.Server{
         Addr: ":8080",
         Handler: mux,
     }
+
+    slog.Info("Starting to listen on", "port", s.Addr)
 
     err := s.ListenAndServe()
     if err != nil {
